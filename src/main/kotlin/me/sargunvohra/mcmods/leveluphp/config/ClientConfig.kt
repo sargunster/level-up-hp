0c788659d807b13f41f5e99b60c56ca1e0f5bbe2
Sargun Vohra <sargun.vohra@gmail.com>
2019-04-23 01:04:14 -0700

    fix config gui

--- src/main/kotlin/me/sargunvohra/mcmods/leveluphp/config/ClientConfig.kt
+++ src/main/kotlin/me/sargunvohra/mcmods/leveluphp/config/ClientConfig.kt
@@ -4,6 +4,7 @@ import me.sargunvohra.mcmods.autoconfig1.ConfigData
 import me.sargunvohra.mcmods.autoconfig1.annotation.Config
 
 @Config(name = "leveluphp")
+@Config.Gui.Background("textures/block/spruce_planks.png")
 data class ClientConfig (
     val enableXpBarOverride: Boolean = true
 ) : ConfigData {
