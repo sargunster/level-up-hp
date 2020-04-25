b07f6cbd4a9f5fd12846fd87fcbec0cafad1dd75
Sargun Vohra <sargun.vohra@gmail.com>
2020-04-25 01:27:53 -0700

    refactor levelling config loading and make it not synchronous

--- src/main/kotlin/me/sargunvohra/mcmods/leveluphp/config/LevellingConfigLoader.kt
+++ src/main/kotlin/me/sargunvohra/mcmods/leveluphp/config/LevellingConfigLoader.kt
@@ -1,53 +1,64 @@
-@file:Suppress("DEPRECATION")
-
 package me.sargunvohra.mcmods.leveluphp.config
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import me.sargunvohra.mcmods.leveluphp.LuhpIds
 import me.sargunvohra.mcmods.leveluphp.hpLevelHandler
+import net.minecraft.client.resources.ReloadListener
+import net.minecraft.profiler.IProfiler
 import net.minecraft.resources.IResourceManager
-import net.minecraft.resources.IResourceManagerReloadListener
 import net.minecraft.server.MinecraftServer
 import org.apache.logging.log4j.LogManager
 import java.io.InputStreamReader
 
-class LevellingConfigLoader(val server: MinecraftServer) : IResourceManagerReloadListener {
+class LevellingConfigLoader(
+    private val server: MinecraftServer
+) : ReloadListener<LevellingConfig>() {
 
-    private val gson = Gson()
-
-    override fun onResourceManagerReload(
-        resourceManager: IResourceManager
-    ) {
-
-        val overrides = resourceManager
-            .getAllResourceLocations("leveluphp/override") { it.endsWith(".json") }
-            .fold(HashMap<String, Int>()) { map, resource ->
-                val override: Map<String, Int> = gson.fromJson(
-                    InputStreamReader(resourceManager.getResource(resource).inputStream),
-                    object : TypeToken<Map<String, Int>>() {}.type
-                )
-                map += override
-                return@fold map
+    private fun prepareOverrides(resourceManager: IResourceManager): Map<String, Int> {
+        val mapOfStrToIntType = object : TypeToken<Map<String, Int>>() {}.type
+        return resourceManager
+            .getAllResourceLocations("leveluphp/override") { path ->
+                path.endsWith(".json")
             }
+            .map { id ->
+                val resource = resourceManager.getResource(id)
+                return@map gson.fromJson<Map<String, Int>>(
+                    InputStreamReader(resource.inputStream),
+                    mapOfStrToIntType
+                )
+            }
+            .fold(HashMap()) { acc, overrides ->
+                acc += overrides
+                return@fold acc
+            }
+    }
 
-        val general = resourceManager.getResource(LuhpIds.LEVELLING_CONFIG)
-        val config = gson.fromJson(
-            InputStreamReader(general.inputStream),
+    override fun prepare(resourceManager: IResourceManager, profiler: IProfiler): LevellingConfig {
+        val overrides = prepareOverrides(resourceManager)
+        val baseConfig = resourceManager.getResource(LuhpIds.LEVELLING_CONFIG)
+        return gson.fromJson(
+            InputStreamReader(baseConfig.inputStream),
             LevellingConfig::class.java
         ).copy(overrides = overrides)
+    }
 
-        LogManager.getLogger().info("Loaded {}", config)
-
-        config.validate()
-        LevellingConfigLoader.config = config
+    override fun apply(
+        loadedConfig: LevellingConfig,
+        resourceManager: IResourceManager,
+        profiler: IProfiler
+    ) {
+        LogManager.getLogger().info("Loaded {}", loadedConfig)
+        loadedConfig.validate()
+        config = loadedConfig
         successfullyLoadedDataPack = true
-
         server.playerList.players.forEach { it.hpLevelHandler.onModified() }
     }
 
     companion object {
         var config: LevellingConfig = LevellingConfig()
+
+        private val gson = Gson()
         private var successfullyLoadedDataPack = false
 
         init {
