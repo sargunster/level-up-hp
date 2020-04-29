9b05cd70289fc6fe2f661fd2591621f3da371ea1
Sargun Vohra <sargun.vohra@gmail.com>
2020-04-28 18:56:30 -0700

    fix client config

--- src/main/kotlin/me/sargunvohra/mcmods/leveluphp/config/ConfigRegistrationSubscriber.kt
+++ src/main/kotlin/me/sargunvohra/mcmods/leveluphp/config/ConfigRegistrationSubscriber.kt
@@ -9,16 +9,6 @@ import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 object ConfigRegistrationSubscriber {
-
-    @SubscribeEvent
-    fun registerClientConfig(event: FMLClientSetupEvent) {
-        ModLoadingContext.get().registerConfig(
-            ModConfig.Type.CLIENT,
-            ClientConfig.spec,
-            "leveluphp.toml"
-        )
-    }
-
     @SubscribeEvent
     fun registerDataConfig(event: FMLServerAboutToStartEvent) {
         event.server.resourceManager.addReloadListener(LevellingConfigManager(event.server))
